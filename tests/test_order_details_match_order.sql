3f3923dc465498ba7bc5d0a5452351f40e445536
Boroumand Niazi <boroumand.niazi@gmail.com>
2024-07-09 13:18:01 -0400

    Custom singular test

---
+++ tests/test_order_details_match_order.sql
@@ -0,0 +1,26 @@
+
+{# {{
+  config(
+    severity = 'warn',
+    )
+}} #}
+with order_details as
+(
+    select
+        order_id,
+        count(*) as num_of_items_in_order
+    from {{ref("stg_ecommerce__order_items")}}
+    group by 1
+)
+
+select
+    o.order_id,
+    o.num_items_ordered,
+    od.num_of_items_in_order,
+
+from {{ref("stg_ecommerce__orders")}} o
+full outer join order_details od using(order_id)
+where
+    o.order_id is null
+    OR od.order_id is null
+    OR o.num_items_ordered != od.num_of_items_in_order
